0a0a308835cdfb340854de4eeab1486a6e1ecbe9
mj00100 <81398574+mj00100@users.noreply.github.com>
2026-06-24 12:07:15 +0800

    GUI Implementation (#8)

---
+++ build/CMakeFiles/BloodDonationEligibilityChecker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BloodDonationEligibilityChecker.exe"
+  "BloodDonationEligibilityChecker.exe.manifest"
+  "BloodDonationEligibilityChecker.pdb"
+  "CMakeFiles/BloodDonationEligibilityChecker.dir/main.c.obj"
+  "CMakeFiles/BloodDonationEligibilityChecker.dir/main.c.obj.d"
+  "libBloodDonationEligibilityChecker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BloodDonationEligibilityChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
